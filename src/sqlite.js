ac0754e8651cd6c9fc08486e38f077ad9cf4c372
Draw-Memory <155839102+Draw-Memory@users.noreply.github.com>
2024-01-19 18:14:12 +0000

    Update sqlite.js

--- src/sqlite.js
+++ src/sqlite.js
@@ -57,7 +57,7 @@ module.exports = {
         // Build the user data from the front-end 
         // and the current time into the sql query
         // await db.run("INSERT INTO Desenhos (time) VALUES (?)", [memorize]);
-        await db.run("INSERT INTO Desenhos (time) VALUES (?)", "ggg");        
+        await db.run("INSERT INTO Desenhos (time) VALUES (?)", ["ggg"]);        
       }
 
       return await db.all("SELECT * from Desenhos");
